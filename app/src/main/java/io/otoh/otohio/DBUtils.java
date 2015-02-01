5bacea8b31deb0be4ddd2923dca3150011ac6e63
Jason Callaway <jason@jasoncallaway.com>
2015-02-01 17:42:26 -0500

    more work on Identities interface, not finished

--- app/src/main/java/io/otoh/otohio/DBUtils.java
+++ app/src/main/java/io/otoh/otohio/DBUtils.java
@@ -20,11 +20,11 @@ public class DBUtils extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("PRAGMA foreign_keys=ON");
 
-        String query = "CREATE TABLE identities (name TEXT PRIMARY KEY, alias TEXT)";
+        String query = "CREATE TABLE IF NOT EXIST identities (name TEXT PRIMARY KEY, alias TEXT)";
         db.execSQL(query);
-        query = "CREATE TABLE certs (name TEXT PRIMARY KEY, identityName TEXT, path TEXT, keyUse TEXT, FOREIGN KEY (identityName) REFERENCES identities (name)) ON DELETE CASCADE";
+        query = "CREATE TABLE IF NOT EXIST certs (name TEXT PRIMARY KEY, identityName TEXT, path TEXT, keyUse TEXT, FOREIGN KEY (identityName) REFERENCES identities (name)) ON DELETE CASCADE";
         db.execSQL(query);
-        query = "CREATE TABLE keyrings (name TEXT PRIMARY KEY, identityName TEXT, path TEXT, type TEXT, fingerprint TEXT, FOREIGN KEY (identityName) REFERENCES identities (name)) ON DELETE CASCADE";
+        query = "CREATE TABLE IF NOT EXIST keyrings (name TEXT PRIMARY KEY, identityName TEXT, path TEXT, type TEXT, fingerprint TEXT, FOREIGN KEY (identityName) REFERENCES identities (name)) ON DELETE CASCADE";
         db.execSQL(query);
 
     }
@@ -40,6 +40,8 @@ public class DBUtils extends SQLiteOpenHelper {
 
         onCreate(db);
     }
+
+    // TODO: write database dump and restore logic
 
     @Override
     public void onOpen(SQLiteDatabase db) {
--- app/src/main/java/io/otoh/otohio/IdentitiesActivity.java
+++ app/src/main/java/io/otoh/otohio/IdentitiesActivity.java
@@ -1,6 +1,7 @@
 package io.otoh.otohio;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -10,10 +11,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
-public class IdentitiesActivity extends Activity implements AdapterView.OnItemClickListener {
+public class IdentitiesActivity extends ListActivity implements AdapterView.OnItemClickListener {
+
+    Intent intent;
+    TextView identityName;
+    DBUtils dbUtils = new DBUtils(this);
 
     // Navigation menu start
     private DrawerLayout drawerLayout;
@@ -67,31 +78,55 @@ public class IdentitiesActivity extends Activity implements AdapterView.OnItemCl
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_identities);
-    }
-
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_identities, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        if(drawerListener.onOptionsItemSelected(item)) { return true; }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerListener.syncState();
+    }
+    // Navigation menu end
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identities);
+
+        setTitle("Your Identities");
+
+        ArrayList<HashMap<String, String>> identitiesList = dbUtils.getIdentities();
+        ListView listView = getListView();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View view, int arg2, long arg3) {
+                identityName = (TextView) view.findViewById(R.id.identity_name);
+                String identityNameValue = identityName.getText().toString();
+                Intent intent = new Intent(getApplication(), IdentityActivity.class);
+                intent.putExtra("identityName", identityNameValue);
+                startActivity(intent);
+            }
+        });
+
+        ListAdapter adapter = new SimpleAdapter(
+                IdentitiesActivity.this, identitiesList, R.layout.identities_entry,
+                new String[] {"identityName"},
+                new int[] {R.id.identity_name});
+
+        setListAdapter(adapter);
+
+
+    }
+
+
+
 }
